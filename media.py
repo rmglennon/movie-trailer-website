be68c952e7f40749ca9cc26f74336f45ec92279b
Rhonda Glennon <rhonda@mapzen.com>
2017-08-06 15:33:55 -0700

    Add Python files

---
+++ media.py
@@ -0,0 +1,19 @@
+# Rhonda Glennon - Movie trailer website for Udacity Full Stack Web Developer project 1
+# fresh_tomatoes.py from https://github.com/udacity/ud036_StarterCode
+
+# import webbrowser module to open system browser
+import webbrowser
+
+# defines the Movie class
+
+class Movie():
+    # constructor defines properties about the movie and sets instance variables
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    # define instance method to open browser and play the movie trailer
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
